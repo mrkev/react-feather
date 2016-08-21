0d3fce8dda2eb1461fc33e59dd75c7dd9fbcfaeb
Kevin <mr.kev@me.com>
2016-08-20 22:58:19 -0700

    add permalink pagination

--- layout.js
+++ layout.js
@@ -285,6 +285,23 @@ window.Pagination = React.createClass({
         <a href={this.props.PreviousPage} id="nav-prev" style={{textTransform:"lowercase"}}>{this.props["lang:Newer"]} &rarr;</a>
       }
 
+    </div>
+  );}
+});
+
+window.PermalinkPagination = React.createClass({
+  render: function() { return (
+    <div id="navigation">
+
+      {!!this.props.PreviousPost &&
+        <a href={this.props.PreviousPost} id="nav-next" style={{textTransform:"lowercase"}}>&larr; {this.props["lang:Older"]}</a>
+      }
+
+      <span class="page_number"> &nbsp; {this.props.CurrentPage}/{this.props.TotalPages} &nbsp;</span>
+
+      {!!this.props.NextPost &&
+        <a href={this.props.NextPost} id="nav-prev" style={{textTransform:"lowercase"}}>{this.props["lang:Newer"]} &rarr;</a>
+      }
 
     </div>
   );}

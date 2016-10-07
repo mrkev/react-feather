25be4c7eb343b8189873d6407ff3c464eff43bac
Kevin <mr.kev@me.com>
2016-10-07 01:49:11 -0400

    Apparence configuration through JS

---
+++ config.js
@@ -0,0 +1,13 @@
+window.config = {
+  "color:Background"      : "#ffffff",
+  "color:Text"            : "#919191",
+  "color:Links"           : "#6dc0a9",
+  "color:Links Hover"     : "#4a8d75",
+  "color:Heading"         : "#000000",
+  "color:Heading Hover"   : "#000000",
+  "color:Description"     : "#919191",
+  "color:Permalink"       : "#3f3f3f",
+  "color:Permalink Hover" : "#000000",
+  "color:Text Alt"        : "#919191",
+  "CustomCSS"             : "",
+}
